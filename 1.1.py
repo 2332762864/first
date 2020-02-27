c06b2ee65bc2bd49d5d776b996af5fd09b206390
2332762864 <61529841+2332762864@users.noreply.github.com>
2020-02-27 17:05:20 +0800

    上传文件
    试试

---
+++ 1.1.py
@@ -0,0 +1,8 @@
+print("                       猜数字游戏                         ")
+temp=input("please enter the number what i though now \n")
+guess=int(temp)
+if guess==9:
+    print("right\nbut nothing")
+else:print("fault")
+
+    
